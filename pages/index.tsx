cdb3bd95a7b610bbb738fd4b8bddff63237b6c6d
Igor Bedesqui <igorbedesqui@gmail.com>
2021-03-03 22:08:35 -0300

    feat: basic implementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,61 @@
-import Head from 'next/head'
+const ffmpeg = createFFmpeg({ log: true });
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const [ready, setReady] = useState(false);
+  const [video, setVideo] = useState<File | null | undefined>();
+  const [gif, setGif] = useState<string>();
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  };
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  const convertToGif = async () => {
+    // if the video exists, write the file to memory
+    video && ffmpeg.FS("writeFile", "test.mp4", await fetchFile(video));
+
+    // Run the FFMpeg command
+    await ffmpeg.run("-i", "test.mp4", "-t", "2", "-f", "gif", "out.gif");
+
+    // Read the result
+    const data = ffmpeg.FS("readFile", "out.gif");
+
+    // Create a URL
+    const url = URL.createObjectURL(
+      new Blob([data.buffer], { type: "image/gif" })
+    );
+    setGif(url);
+  };
+
+  return ready ? (
+    <div className="App">
+      {video && (
+        <video controls width="250" src={URL.createObjectURL(video)}></video>
+      )}
+
+      <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
+
+      <h3>Result</h3>
+
+      <button onClick={convertToGif}>Convert</button>
+
+      {gif && (
+        <>
+          <img src={gif} width="250" />
+          <a href={gif} download="hej">
+            Download
           </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+        </>
+      )}
     </div>
-  )
+  ) : (
+    <p>Loading...</p>
+  );
 }
+
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+import { useState, useEffect } from "react";
