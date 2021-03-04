39ac8f34e40460704ca496c5faecd6eb51455570
Igor Bedesqui <igorbedesqui@gmail.com>
2021-03-04 19:49:02 -0300

    fix: reverted last commit

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
-export async function headers() {
-  return [
-    {
-      source: "/",
-      headers: [
-        {
-          key: "Cross-Origin-Opener-Policy",
-          value: "same-origin",
-        },
-        {
-          key: "Cross-Origin-Embedder-Policy",
-          value: "require-corp",
-        },
-      ],
-    },
-  ];
-}
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+        ],
+      },
+    ];
+  },
+};
