eb6b265ff907fbedf939e99248e6e6b0c7aad47d
Igor Bedesqui <igorbedesqui@gmail.com>
2021-03-15 19:44:46 -0300

    feat: configured colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      gray: colors.warmGray,
+    },
+    extend: {
+      colors: {
+        igor: {
+          light: "#fffcf5",
+          500: "#473539",
+          700: "#2e2225",
+        },
+      },
+    },
+    fontFamily: {
+      sans: ["Mplus", ...fontFamily.sans],
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
